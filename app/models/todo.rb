afebb81b9299a7b8e4c6b74436ab589fbdb76f82
hibik <hbtats@gmail.com>
2024-06-16 16:45:57 +0900

    customのvalidationを作成して適用

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,19 @@
 class Todo < ApplicationRecord
+  validates :title_length_within_bounds
+  validates :description_length_within_bounds
+
+  private
+
+  def title_length_within_bounds
+    if title.length < 5 || title.length > 20
+      errors.add(:title, "タイトルは5文字以上20文字以内で作成してください")
+    end
+  end
+
+  def description_length_within_bounds
+    if description.length < 50
+      errors.add(:description, "詳細は50文字以内で作成してください")
+    end
+  end
+
 end
